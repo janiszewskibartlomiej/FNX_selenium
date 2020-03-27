a8fdf140c4864b45175e8d9ab7296fb6b1c823a4
janiszewskibartlomiej <janiszewski.bartlomiej@gmail.com>
2020-03-27 11:04:00 +0100

    added work

--- Hipp9/#11416_captcha_test.py
+++ Hipp9/#11416_captcha_test.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 import unittest, time, re
@@ -11,17 +12,31 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 from Auth import Url, Login, Password
 
-CAPTCHA_ELEMENT = expected_conditions.presence_of_element_located((By.ID, "recaptcha-anchor-label"))
-DELAY = 3
 
 class CaptchaTestCase(unittest.TestCase):
     """
     Test chaecked  captch functionality
 
+    Steps:
+
+    1. Go to login page
+    2. Try to login - first time
+    3. Try to login - second time
+    4. Try to login - third time
+    5. Verifying is captcha show
+    6. Try accept captcha
+    7. Try to go login page
+    8. Verifying is captcha show after one incorect login
+    9. Try accept captcha
+    10. Try to go login page
     """
 
     def setUp(self):
         self.driver = webdriver.Firefox()
+        # options = webdriver.ChromeOptions()
+        # options.add_argument('--ignore-certificate-errors')
+        # self.driver = webdriver.Chrome('C:/Users/janis/PycharmProjects/FNX_selenium/Driver/chromedriver.exe',
+        #                                chrome_options=options)
         self.driver.fullscreen_window()
         # self.driver.maximize_window()
         self.driver.implicitly_wait(30)
@@ -82,7 +97,7 @@ class CaptchaTestCase(unittest.TestCase):
         driver.find_element_by_xpath("//input[@placeholder='Login']").send_keys("ktq@da.pl")
 
         driver.find_element_by_xpath(u"//input[@placeholder='Hasło']").clear()
-        driver.find_element_by_xpath(u"//input[@placeholder='Hasło']").send_keys("test"+Keys.ENTER)
+        driver.find_element_by_xpath(u"//input[@placeholder='Hasło']").send_keys("test" + Keys.ENTER)
 
         # try to login
         try:
@@ -93,14 +108,25 @@ class CaptchaTestCase(unittest.TestCase):
         time.sleep(2)
         # check is captcha visible
         try:
-            self.assertEqual("Nie jestem robotem", driver.find_element_by_id("recaptcha-anchor-label").text)
+            element = WebDriverWait(driver, 10).until(
+                EC.visibility_of_element_located((By.XPATH, "//div[@class='g-recaptcha]'")))
+            # print(driver.find_element_by_xpath("//div[@class='g-recaptcha']"))
+            self.assertEqual("reCAPTCHA", driver.find_element_by_xpath("//div[@class='g-recaptcha']"))
+        except AssertionError as e:
+            self.verificationErrors.append(str(e))
+
+        # verifying current url
+        try:
+            current_url = driver.current_url
+            self.assertEqual(current_url, Url.HIPP_VAL)
+            # print(current_url)
         except AssertionError as e:
             self.verificationErrors.append(str(e))
 
         # click on checkbox in captcha
+        time.sleep(2)
         try:
-            WebDriverWait(driver, DELAY).until(CAPTCHA_ELEMENT)
-            driver.find_element_by_xpath("//span[@id='recaptcha-anchor']/div").click()
+            driver.find_element_by_xpath("//div[@class='g-recaptcha']").click()
         except AssertionError as e:
             self.verificationErrors.append(str(e))
 
@@ -134,20 +160,21 @@ class CaptchaTestCase(unittest.TestCase):
         time.sleep(2)
         # check is captcha visible
         try:
-            self.assertEqual("Nie jestem robotem", driver.find_element_by_id("recaptcha-anchor-label").text)
+            self.assertEqual("reCAPTCHA", driver.findElement(By.xpath("//div[@class='content_element']//img")))
         except AssertionError as e:
             self.verificationErrors.append(str(e))
 
         # click on checkbox in captcha
         try:
-            driver.find_element_by_xpath("//span[@id='recaptcha-anchor']/div").click()
+            # WebDriverWait(driver, DELAY).until(CAPTCHA_ELEMENT)
+            driver.findElement(By.xpath("//div[@class='content_element']//img").click())
         except AssertionError as e:
             self.verificationErrors.append(str(e))
 
         time.sleep(2)
         # click on 'login' button
         try:
-            driver.find_element_by_name("login").click()
+            self.driver.find_element_by_name("login").click()
         except AssertionError as e:
             self.verificationErrors.append(str(e))
 
@@ -166,6 +193,7 @@ class CaptchaTestCase(unittest.TestCase):
 
     def tearDown(self):
         self.driver.close()
+        self.driver.quit()
 
 
 if __name__ == "__main__":
